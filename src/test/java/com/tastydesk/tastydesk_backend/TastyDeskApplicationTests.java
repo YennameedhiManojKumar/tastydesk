1e27a05ede504ef74122c6f707cd9ee0c97bebfc
Manoj <manojkumar944140@gmail.com>
2025-07-07 23:59:29 +0530

    initialised spring dependencies

---
+++ src/test/java/com/tastydesk/tastydesk_backend/TastyDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastydesk.tastydesk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
